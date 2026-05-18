febc8a1dc85391f469fa9d65bdaca9844ef80d6c
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-18 12:58:19 +0530

    Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3

---
+++ bronze_tables.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# ## Bronze tables creation
+
+# CELL ********************
+
+from pyspark.sql import functions as F
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+a = 1 
+b = 2 
+print(a//b)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
