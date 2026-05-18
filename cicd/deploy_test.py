002395bba05a8971ffc8e860c7797abbaa472eec
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-18 17:15:03 +0530

    Update deploy_test.py

--- cicd/deploy_test.py
+++ cicd/deploy_test.py
@@ -8,7 +8,7 @@ from fabric_cicd import (
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-WORKSPACE_ID = os.getenv("TEST_WORKSPACE_ID")
+TEST_WORKSPACE_ID = os.getenv("TEST_WORKSPACE_ID")
 credential = ClientSecretCredential(
    tenant_id=TENANT_ID,
    client_id=CLIENT_ID,
@@ -27,5 +27,5 @@ target_workspace = FabricWorkspace(
    token_credential=credential
 )
 publish_all_items(target_workspace)
-unpublish_all_orphan_items(target_workspace)
+#unpublish_all_orphan_items(target_workspace)
 print("Deployment to TEST completed successfully")
