d27ad43ac6e64e6d92e054e6b4732bcbd5f1b544
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-18 13:29:29 +0530

    Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3

--- bronze_tables.Notebook/notebook-content.py
+++ bronze_tables.Notebook/notebook-content.py
@@ -10,13 +10,60 @@
 
 # MARKDOWN ********************
 
+# Fabric notebook source
+
+# MARKDOWN ********************
+
+# METADATA ********************
+
+# MARKDOWN ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "e3a21b57-160a-49bf-9a49-8d50ac00835b",
+# META       "default_lakehouse_name": "raw",
+# META       "default_lakehouse_workspace_id": "fd090d74-08b5-4348-89a8-15d1280f84b3",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "e3a21b57-160a-49bf-9a49-8d50ac00835b"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# MARKDOWN ********************
+
+# MARKDOWN ********************
+
 # ## Bronze tables creation
+
+# MARKDOWN ********************
+
+# CELL ********************
 
 # CELL ********************
 
 from pyspark.sql import functions as F
 
 # METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# MARKDOWN ********************
+
+# METADATA ********************
+
+# MARKDOWN ********************
 
 # META {
 # META   "language": "python",
