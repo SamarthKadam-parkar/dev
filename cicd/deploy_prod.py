3bd7e8a19807ef5f126ba81f8d0f87e03614ad50
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-18 12:30:24 +0530

    modified .py and .yml files

---
+++ cicd/deploy_prod.py
@@ -0,0 +1,29 @@
+import os
+from azure.identity import ClientSecretCredential
+from fabric_cicd import (
+   FabricWorkspace,
+   publish_all_items
+)
+TENANT_ID = os.getenv("FABRIC_TENANT_ID")
+CLIENT_ID = os.getenv("FABRIC_CLIENT_ID")
+CLIENT_SECRET = os.getenv("FABRIC_CLIENT_SECRET")
+WORKSPACE_ID = os.getenv("FABRIC_PROD_WORKSPACE_ID")
+credential = ClientSecretCredential(
+   tenant_id=TENANT_ID,
+   client_id=CLIENT_ID,
+   client_secret=CLIENT_SECRET
+)
+target_workspace = FabricWorkspace(
+   workspace_id=WORKSPACE_ID,
+   repository_directory="./",
+   item_type_in_scope=[
+       "Notebook",
+       "DataPipeline",
+       "Lakehouse",
+       "SemanticModel",
+       "Report"
+   ],
+   token_credential=credential
+)
+publish_all_items(target_workspace)
+print("Deployment to PROD completed successfully")
