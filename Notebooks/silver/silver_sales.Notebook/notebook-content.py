76f0071eaf6b2ee6c7fbc85ef3e9edf60485983b
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-21 16:06:20 +0530

    Dev (#41)
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6 (#26)
    
    * Feature 26.5.3 (#29)
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * Feature 26.5.3 (#30)
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * fixed deploy-test.yml file
    
    * Feature 26.5.3 (#31)
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * fixed deploy-test.yml file
    
    * fixed deploy_test.py
    
    * Feature 26.5.3 (#33)
    
    * Update deploy-test.yml
    
    * Update deploy-test.yml
    
    * Update deploy_test.py
    
    * Update deploy_test.py
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6 (#26) (#28)
    
    * Committing 1 item from workspace b87e00d4-471f-4799-be95-2a0283f056d2
    
    * Committing 1 item from workspace b87e00d4-471f-4799-be95-2a0283f056d2
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6 (#26) (#27)
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * fixed deploy-test.yml file
    
    * Update deploy-test.yml
    
    * fixed deploy_test.py
    
    * Update deploy_test.py
    
    * Feature 26.5.3 (#36)
    
    * Update deploy-test.yml
    
    * Update deploy-test.yml
    
    * Update deploy_test.py
    
    * Update deploy_test.py
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6 (#26) (#28)
    
    * Committing 1 item from workspace b87e00d4-471f-4799-be95-2a0283f056d2
    
    * Committing 1 item from workspace b87e00d4-471f-4799-be95-2a0283f056d2
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6 (#26) (#27)
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * fixed deploy-test.yml file
    
    * Update deploy-test.yml
    
    * fixed deploy_test.py
    
    * Update deploy_test.py
    
    * Feature 26.5.3 (#37)
    
    * Committing 1 item from workspace 65ba8069-c339-4361-a3f3-09d20f3dd8c6
    
    * Adding gold notebook for products table
    
    * updated prod deployment related files
    
    * nc
    
    * fixed deploy-test.yml file
    
    * fixed deploy_test.py
    
    * fixed .yml files
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3 (#38)
    
    * Feature skadam (#40)
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3

--- Notebooks/silver/silver_sales.Notebook/notebook-content.py
+++ Notebooks/silver/silver_sales.Notebook/notebook-content.py
@@ -8,12 +8,15 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "11097028-1ac9-4f23-8c36-f279b0a66988",
-# META       "default_lakehouse_name": "bronze",
+# META       "default_lakehouse": "b09a12ed-928e-4d44-80c2-556c1a743dcd",
+# META       "default_lakehouse_name": "silver",
 # META       "default_lakehouse_workspace_id": "fd090d74-08b5-4348-89a8-15d1280f84b3",
 # META       "known_lakehouses": [
 # META         {
 # META           "id": "11097028-1ac9-4f23-8c36-f279b0a66988"
+# META         },
+# META         {
+# META           "id": "b09a12ed-928e-4d44-80c2-556c1a743dcd"
 # META         }
 # META       ]
 # META     }
@@ -39,7 +42,7 @@ from pyspark.sql import functions as F
 # CELL ********************
 
 TARGET_TABLE = 'products'
-TARGET_SCHEMA = 'silver'
+SOURCE_LAKEHOUSE = 'bronze'
 
 # METADATA ********************
 
@@ -50,7 +53,7 @@ TARGET_SCHEMA = 'silver'
 
 # CELL ********************
 
-products = spark.read.table("bronze.products")
+products = spark.read.table(f"{SOURCE_LAKEHOUSE}.products")
 
 # METADATA ********************
 
@@ -62,14 +65,15 @@ products = spark.read.table("bronze.products")
 # CELL ********************
 
 products = (products
-.withColumn("PRODUCT_NAME",F.upper(F.col("PRODUCT_NAME")))
-.withColumn("CATEGORY",F.upper(F.col("CATEGORY")))
+.withColumn("ProductName",F.upper(F.col("ProductName")))
+.withColumn("Category",F.upper(F.col("Category")))
 .select(
-    F.col("PRODUCT_ID"),
-    F.col("PRODUCT_NAME"),
-    F.col("CATEGORY"),
-    F.col("PRICE")
-))
+    F.col("ProductID").alias("PRODUCT_ID"),
+    F.col("ProductName").alias("PRODUCT_NAME"),
+    F.col("Category").alias("CATEGORY"),
+    F.col("Price").alias("PRICE")
+)
+)
 
 # METADATA ********************
 
@@ -82,10 +86,15 @@ products = (products
 
 products.write\
 .format('delta')\
-.saveAsTable(f"{TARGET_SCHEMA}.{TARGET_TABLE}")
+.mode('overwrite')\
+.saveAsTable(f"{TARGET_TABLE}")
 
 # METADATA ********************
 
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
 # META {
 # META   "language": "python",
 # META   "language_group": "synapse_pyspark"
