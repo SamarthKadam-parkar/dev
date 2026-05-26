6e98ff96b2db818535feb8d6d5a05e6cf9483554
SamarthKadam-parkar <skadam1@parkar.in>
2026-05-26 13:55:27 +0530

    Feature skadam (#77)
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * silver_sales lakehouses removed
    
    * removed lakehouse metadata for brinze notebook
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * defined parameter.yml
    
    * hotfix_1 for parameter.yml
    
    * scrapped parameter.yml
    
    * removed "lakehouses" as items_in_scope to deploy
    in target workspaces
    
    * Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * added parameter.yml for semantic model
    
    * edited connection id var
    
    * Committing 3 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * changed name for semantic model
    
    * changed parameter cred
    
    * nc
    
    * nc
    
    * updated parameter.yml connection ID fix
    
    * nc
    
    * updated parameter.yml
    
    * updated parameter.yml
    
    * fixed parameter.yml
    
    * updated prameter.yml
    
    * nc
    
    * added test guid
    
    * nc
    
    * fixed semantic model name
    
    * added dev and test
    
    * nc
    
    * nc
    
    * nc
    
    * implemented publishing only the changed items

--- cicd/deploy_prod.py
+++ cicd/deploy_prod.py
@@ -1,21 +1,26 @@
 import os
 from azure.identity import ClientSecretCredential
+
 from fabric_cicd import (
    FabricWorkspace,
-   publish_all_items
+   publish_all_items,
+   get_changed_items
 )
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 PROD_WORKSPACE_ID = os.getenv("PROD_WORKSPACE_ID")
+
 credential = ClientSecretCredential(
    tenant_id=TENANT_ID,
    client_id=CLIENT_ID,
    client_secret=CLIENT_SECRET
 )
+
 target_workspace = FabricWorkspace(
    workspace_id=PROD_WORKSPACE_ID,
    repository_directory="./",
+   environment="prod",
    item_type_in_scope=[
        "Notebook",
        "DataPipeline",
@@ -24,5 +29,10 @@ target_workspace = FabricWorkspace(
    ],
    token_credential=credential
 )
-publish_all_items(target_workspace)
-print("Deployment to PROD completed successfully")
+
+changed = get_changed_items(target_workspace.repository_directory)
+if changed:
+   publish_all_items(target_workspace,items_to_include=changed)
+   print("Deployment to PROD completed successfully")
+else:
+   print("No fabric items were added, modified or removed for current push from test to prod")
--- cicd/deploy_test.py
+++ cicd/deploy_test.py
@@ -1,24 +1,31 @@
 import os
 from azure.identity import ClientSecretCredential
+
+from dotenv import load_dotenv
+load_dotenv()
+
 from fabric_cicd import (
    FabricWorkspace,
    publish_all_items,
-   unpublish_all_orphan_items
+   get_changed_items
 )
+
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TEST_WORKSPACE_ID = os.getenv("TEST_WORKSPACE_ID")
+
 credential = ClientSecretCredential(
    tenant_id=TENANT_ID,
    client_id=CLIENT_ID,
    client_secret=CLIENT_SECRET
 )
+
 target_workspace = FabricWorkspace(
    workspace_id=TEST_WORKSPACE_ID,
    repository_directory="./",
    environment="test",
-   item_type_in_scope=[
+   item_type_in_scope= [
        "Notebook",
        "DataPipeline",
        "SemanticModel",
@@ -26,6 +33,12 @@ target_workspace = FabricWorkspace(
    ],
    token_credential=credential
 )
-publish_all_items(target_workspace)
-# unpublish_all_orphan_items(target_workspace)
-print("Deployment to TEST completed successfully")
+
+changed = get_changed_items(target_workspace.repository_directory)
+if changed:
+    publish_all_items(target_workspace,items_to_include=changed)
+    print("Deployment to TEST completed successfully")
+else:
+    print("No fabric items were added, modified or removed for current push from dev to test")
+
+
