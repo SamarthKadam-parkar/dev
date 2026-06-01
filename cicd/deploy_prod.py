7b1c857cbbb0b2438fa2802aeaa71ff9b1893483
SamarthKadam-parkar <skadam1@parkar.in>
2026-06-01 16:17:04 +0530

    Feature skadam (#124)
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * silver_sales lakehouses removed
    
    * removed lakehouse metadata for brinze notebook
    
    * Committing 1 item from workspace d69c1321-f83e-4b0f-86f7-5b06be4bff06
    
    * defined parameter.yml
    
    * hotfix_1 for parameter.yml
    
    * scrapped parameter.yml
    
    * removed "lakehouses" as items_in_scope to deploy
    in target workspaces
    
    * Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * added parameter.yml for semantic model
    
    * edited connection id var
    
    * Committing 3 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * changed name for semantic model
    
    * changed parameter cred
    
    * nc
    
    * nc
    
    * updated parameter.yml connection ID fix
    
    * nc
    
    * updated parameter.yml
    
    * updated parameter.yml
    
    * fixed parameter.yml
    
    * updated prameter.yml
    
    * nc
    
    * added test guid
    
    * nc
    
    * fixed semantic model name
    
    * added dev and test
    
    * nc
    
    * nc
    
    * nc
    
    * implemented publishing only the changed items
    
    * removed dotenv
    
    * nc
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * fix_1
    
    * nc
    
    * added new logic
    
    * added prod logic
    
    * nc
    
    * Committing 3 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * nc
    
    * nc
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * nc
    
    * nc
    
    * nc
    
    * nc
    
    * nc
    
    * nc
    
    * format updated
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * added prod creds
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * nc
    
    * nc
    
    * nc
    
    * fixed test bronze onelake id
    
    * nc
    
    * nc
    
    * nc
    
    * nc
    
    * replaced all connection names with connection IDs
    
    * Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * Committing 1 item from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * updated yml file from feature_Nikita branch
    
    * commented airflow job and API graphQL
    
    * Committing 2 items from workspace fd090d74-08b5-4348-89a8-15d1280f84b3
    
    * nc
    
    * added selective deployment logic
    
    * added selective deployment logic
    
    * remove dotenv for git
    
    * deployment_1.6.26
    
    * nc
    
    ---------
    
    Co-authored-by: NIKITA DAMBHARE <42292907+Nikita-dambhare@users.noreply.github.com>

--- cicd/deploy_prod.py
+++ cicd/deploy_prod.py
@@ -1,34 +1,45 @@
 import os
 from azure.identity import ClientSecretCredential
-
+import json
 from fabric_cicd import (
    FabricWorkspace,
-   publish_all_items
+   publish_all_items,
+   append_feature_flag
 )
+
+append_feature_flag("enable_experimental_features")
+append_feature_flag("enable_items_to_include")
+
+#azure credentials 
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+#prod workspace ID
 PROD_WORKSPACE_ID = os.getenv("PROD_WORKSPACE_ID")
 
+#Client Secret Credential - Tenant ID + Client ID + Client Secret
 credential = ClientSecretCredential(
    tenant_id=TENANT_ID,
    client_id=CLIENT_ID,
    client_secret=CLIENT_SECRET
 )
 
+#Reading Manifest File to get list of items to deploy in target workspace
+with open("manifest/manifest.json","r") as f:
+    manifest = json.load(f)
+items_in_scope = manifest["items_in_scope"]
+item_type_in_scope = manifest["item_type_in_scope"]
+
+#Initializing Target Workspace
 target_workspace = FabricWorkspace(
    workspace_id=PROD_WORKSPACE_ID,
    repository_directory="./",
    environment="prod",
-   item_type_in_scope=[
-       "Notebook",
-       "DataPipeline",
-       "SemanticModel",
-       "Report"
-   ],
+   item_type_in_scope=item_type_in_scope,
    token_credential=credential
 )
 
-
-publish_all_items(target_workspace,items_to_include=changed)
+#Publishing to Target Workspace
+publish_all_items(target_workspace,items_to_include=items_in_scope)
 print("Deployment to PROD completed successfully")
--- cicd/deploy_test.py
+++ cicd/deploy_test.py
@@ -4,28 +4,34 @@ import json
 from fabric_cicd import (
    FabricWorkspace,
    publish_all_items,
-   unpublish_all_orphan_items,
    append_feature_flag
 )
 
 append_feature_flag("enable_experimental_features")
 append_feature_flag("enable_items_to_include")
 
+#azure credentials 
 TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+#prod workspace ID
 TEST_WORKSPACE_ID = os.getenv("TEST_WORKSPACE_ID")
+
+#Client Secret Credential - Tenant ID + Client ID + Client Secret
 credential = ClientSecretCredential(
    tenant_id=TENANT_ID,
    client_id=CLIENT_ID,
    client_secret=CLIENT_SECRET,
 )
 
+#Reading Manifest File to get list of items to deploy in target workspace
 with open("manifest/manifest.json","r") as f:
     manifest = json.load(f)
 items_in_scope = manifest["items_in_scope"]
 item_type_in_scope = manifest["item_type_in_scope"]
 
+#Initializing Target Workspace
 target_workspace = FabricWorkspace(
    workspace_id=TEST_WORKSPACE_ID,
    repository_directory="./",
@@ -33,9 +39,7 @@ target_workspace = FabricWorkspace(
    item_type_in_scope= item_type_in_scope,
    token_credential=credential,
 )
-publish_all_items(
-                  target_workspace,             
-                  items_to_include=items_in_scope 
-                  )
-# unpublish_all_orphan_items(target_workspace)
+
+#Publishing to Target Workspace
+publish_all_items(target_workspace,items_to_include=items_in_scope)
 print("Deployment to TEST completed successfully")
